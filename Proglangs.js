ce1fcc8413195df33d39e3b6d44cd6373e98d5a3
Gaugen <torabang@gmail.com>
2013-09-10 15:28:28 +0200

    oppdaterte innholdet i svaret

---
+++ Proglangs.js
@@ -0,0 +1,8 @@
+﻿//var språk = document.getElementById("språk");alert(språk);språk.style.color = "green";
+
+function PrintToOutputBox(selectobj){
+var e_proglangs = document.getElementById("proglangs");
+var e_outputbox = document.getElementById("outputbox");
+e_outputbox.innerHTML = "Du har valgt programeringsspråket" + e_proglangs.options[selectobj.selectedIndex]text;
+}
+//github.com/is104/
