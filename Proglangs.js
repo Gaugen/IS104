e03aea070ef02eeb95fcf559d49b3b17f8c8c4a9
Gaugen <torabang@gmail.com>
2013-09-17 16:37:01 +0200

    Update Proglangs.js

--- Proglangs.js
+++ Proglangs.js
@@ -1,8 +1,9 @@
 ﻿//var språk = document.getElementById("språk");alert(språk);språk.style.color = "green";
 
-function PrintToOutputBox(selectobj){
+function printToOutputBox(selectobj){
 var e_proglangs = document.getElementById("proglangs");
 var e_outputbox = document.getElementById("outputbox");
-e_outputbox.innerHTML = "Du har valgt programeringsspråket" + e_proglangs.options[selectobj.selectedIndex]text;
+e_outputbox.innerHTML = "Du har valgt programeringsspråket "  
++ e_proglangs.options[selectobj.selectedIndex].text+"</a>";
 }
 //github.com/is104/
